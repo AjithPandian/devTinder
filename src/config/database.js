44f7f59fef80cb74f209ad50f01b79640354961c
Ajith Pandian <ajithpandian195@gmail.com>
2025-12-24 01:15:20 +0530

    Commit for updating the .env files

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,17 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://namsate_dev_pandian_09:T9st5YYV99AK5iK3@namastenode.bx1igmt.mongodb.net/devTinder"
-    );
+    const mongoURI = process.env.MONGO_URI;
+    if (!mongoURI) {
+      console.error("MONGO_URI is not set in environment");
+      process.exit(1);
+    }
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
   } catch (err) {
     console.error(err.message);
     process.exit(1);
